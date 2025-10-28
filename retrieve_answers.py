ce0589690f5ee85d94a467fb88ca251259b09a3e
Titouan Lozach <titouan.lozach@laposte.net>
2025-10-28 16:13:42 +0100

    experiment 10s size 50

--- retrieve_answers.py
+++ retrieve_answers.py
@@ -22,24 +22,26 @@ def runner_report(filename, nb_terms, initial_term):
 
 def result_kbe(directory, list):
     new_list = []
-    for i in range(5):
-        for j in range(10):
-            for k in range(10):
-                for l in range(10):
-                    number = 1000*i + 100*j + 10*k + l
-                    term, size_caviar = list[number]
-                    filename = directory + "r" + str(i)+str(j)+str(k)+str(l)+".txt.txt"
-                    with open(filename, "r", encoding="utf-8") as f:
-                        contents = f.read()
-                    pos = contents.rfind("Extracted term:\n")
-                    if pos == -1:
-                        new_list.append((term, size_caviar, -1))
-                    else:
-                        # we cut at the last extracted term
-                        res = contents.rsplit("Extracted term:\n", 1)[1]
-                        # we get the term
-                        new_term = res.split("\n", 1)[0]
-                        new_list.append((term, size_caviar, get_size(new_term)))
+    #for i in range(0):
+    for j in range(6):
+        for k in range(10):
+            for l in range(10):
+                number = 100*j + 10*k + l + 1 # 1000*i +
+                if (number > 551):
+                    break
+                term, size_caviar = list[number-2]
+                filename = directory + "10r0" +str(j)+str(k)+str(l)+".txt"
+                with open(filename, "r", encoding="utf-8") as f:
+                    contents = f.read()
+                pos = contents.rfind("Extracted term:\n")
+                if pos == -1:
+                    new_list.append((term, size_caviar, -1))
+                else:
+                    # we cut at the last extracted term
+                    res = contents.rsplit("Extracted term:\n", 1)[1]
+                    # we get the term
+                    new_term = res.split("\n", 1)[0]
+                    new_list.append((term, size_caviar, get_size(new_term)))
     return new_list
 
 
@@ -48,14 +50,14 @@ def result_kbe(directory, list):
 
 
 
-p1 = runner_report("./fixed_caviar_size50/sexpr/caviar_result_1s", 2316, 1)
-p2 = runner_report("./fixed_caviar_size50/sexpr/caviar_result_1s_part2", 1054, 2317)
-p3 = runner_report("./fixed_caviar_size50/sexpr/caviar_result_1s_part3", 1628, 3373)
+#p1 = runner_report("./fixed_caviar_size50/sexpr/caviar_result_1s", 2316, 1)
+#p2 = runner_report("./fixed_caviar_size50/sexpr/caviar_result_1s_part2", 1054, 2317)
+#p3 = runner_report("./fixed_caviar_size50/sexpr/caviar_result_1s_part3", 1628, 3373)
 
-p1.append((2317, -1)) 
-p2.append((3372, -1))             
-caviar_result = (p1 + p2) + p3 
-
+#p1.append((2317, -1)) 
+#p2.append((3372, -1))             
+#caviar_result = (p1 + p2) + p3 
+caviar_result  = runner_report("./fixed_caviar_size50/sexpr/caviar_result_10s", 550, 2)
 endlist = result_kbe("./", caviar_result)
 
 
@@ -95,11 +97,15 @@ def generate_plot(stats, caviar, kbe, total, yaxis_max = 5000):
     ax.set_ylim(0, yaxis_max) 
     ax.set_xlabel("delta kbe-caviar", fontsize=12)
     ax.set_ylabel("number of terms", fontsize=12)
-    ax.set_title("for terms of fixed size 50, 1s", fontsize=14)
+    ax.set_title("for terms of fixed size 50, 10s", fontsize=14)
 
     ax.grid(True, linestyle='--', alpha=0.6)
     plt.tight_layout()
     plt.show()
 
 stats, cause_caviar, cause_kbe, total = filter_count_notusable(endlist)
-generate_plot(stats, cause_caviar, cause_kbe, total, 200)
+generate_plot(stats, cause_caviar, cause_kbe, total, 550)
+
+
+# maybe generate a csv file with the right informations, as
+# the list basically
